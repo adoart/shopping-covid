cc9370c4d97e5a3f1e1f4362e87ccac50e188712
adoart <andre.pereira.duarte@gmail.com>
2021-07-29 09:53:09 +0200

    $11s Add transitions between levels Added crossfade, still buggy and need to implement on other levels (implemented on Level1.unity only)

---
+++ Shopping COVID/Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+    public Animator transition;
+    public float transitionTime = 1.0f;
+    public void LoadNextLevel() {
+        //Load Next Level
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        //Play animation
+        transition.SetTrigger("Start");
+        
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
