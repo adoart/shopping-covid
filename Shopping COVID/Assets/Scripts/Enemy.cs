904364532346d6e3891a2b3de4c39d63f0beab18
adoart <andre.pereira.duarte@gmail.com>
2021-10-29 15:19:06 +0200

    $12e-coughing-scares NPCs start running when infected with covid for a while. Enemies cough randomly and scare NPCs nearby (kinda works, still buggy). TODO: add cough animation...

--- Shopping COVID/Assets/Scripts/Enemy.cs
+++ Shopping COVID/Assets/Scripts/Enemy.cs
@@ -1,6 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Enemy : NPCController {
+    [SerializeField]
+    private float coughingRadius = 10.0f;
+
+    new void Start() {
+        base.Start();
+        StartCoroutine(WaitForNextCough());
+    }
     private void OnCollisionEnter(Collision collision) {
         if (collision.collider.gameObject.CompareTag("Player")) {
             Debug.Log("Collided with Player!!!");
@@ -13,6 +23,29 @@ public class Enemy : NPCController {
         if (other.gameObject.CompareTag("NPC")) {
             NPCController npc = other.gameObject.GetComponent<NPCController>();
             npc.Infect();
+        }
+    }
+
+    protected IEnumerator WaitForNextCough() {
+        yield return new WaitForSeconds(1);
+        Cough();
+        yield return new WaitForSeconds(Random.Range(5, 10));
+        StartCoroutine(WaitForNextCough());
+    }
+
+    public void Cough() {
+        //TODO animation
+        // Debug.Log("Coughed!!!");
+        Collider[] colliders = new Collider[15];
+        Physics.OverlapSphereNonAlloc(transform.position, coughingRadius, colliders);
+        IEnumerable<Collider> queryColliders =
+            from collider in colliders
+            where collider != null && collider.gameObject.CompareTag("NPC")
+            select collider;
+        foreach (Collider other in queryColliders) {
+            Debug.Log("Coughing Scared!!!");
+            NPCController npc = other.gameObject.GetComponent<NPCController>();
+            npc.Scare();
         }
     }
 }
