57b22cfaa7300278a6e75f6e196109640e1f8cfc
adoart <andre.pereira.duarte@gmail.com>
2024-04-16 16:35:05 +0200

    More speed more fun, more controls (keys,gamepad)

--- Shopping COVID/Assets/Scripts/PlayerController.cs
+++ Shopping COVID/Assets/Scripts/PlayerController.cs
@@ -1,122 +1,166 @@
 ﻿using System;
 using System.Collections;
+
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
     #region Events
-
+    
     public event Action HitByEnemy;
-
+    
     public event Action<GameObject> PickedUpItem;
-
+    
     public event Action TriedToExit;
-
+    
     #endregion
-
+    
     public Camera cam;
     public NavMeshAgent agent;
     private Animator playerAnimator;
     private GameManager gameManager;
     private AudioSource audioSource;
+    private ParticleSystem particleSystem;
     [SerializeField] private GameObject destinationIndicator;
     [SerializeField] private GameObject powerupIndicator;
     [SerializeField] private AudioClip powerupPickupSound;
     [SerializeField] private AudioClip itemPickupSound;
     [SerializeField] private GameObject forgotTextBubble;
     [SerializeField] private Vector3 forgotBubbleOffset;
-
+    [SerializeField] private float slipSpeed = 0.2f;
+    [SerializeField] private float walkSpeed = 1.0f;
+    private float verAxis;
+    private float horAxis;
+    
     private bool hasMask;
-
+    private bool isSlipping;
+    
     void Start() {
         audioSource = GetComponent<AudioSource>();
         playerAnimator = GetComponentInChildren<Animator>();
+        particleSystem = GetComponent<ParticleSystem>();
         gameManager = FindObjectOfType<GameManager>();
-        powerupIndicator.SetActive(false);
-        destinationIndicator.SetActive(false);
+        powerupIndicator.SetActive( false );
+        destinationIndicator.SetActive( false );
     }
     // Update is called once per frame
     void Update() {
         MouseMovePlayer();
+        KeysMovePlayer();
         UpdateForgotBubblePosition();
-
+        
         //Update animator speed to control animation.
-        playerAnimator.SetFloat("Speed_f", agent.velocity.magnitude);
+        playerAnimator.SetFloat( "Speed_f", agent.velocity.magnitude );
     }
-
     private void UpdateForgotBubblePosition() {
-        if (forgotTextBubble) {
-            forgotTextBubble.transform.position = cam.WorldToScreenPoint(transform.position + forgotBubbleOffset);
+        if( forgotTextBubble ) {
+            forgotTextBubble.transform.position = cam.WorldToScreenPoint( transform.position + forgotBubbleOffset );
         }
     }
-
-    public void Warp(Vector3 position) {
-        agent.Warp(position);
+    
+    public void Warp( Vector3 position ) {
+        agent.Warp( position );
     }
-
+    
     public void LooseLife() {
-        if (HitByEnemy != null && !hasMask) {
+        if( HitByEnemy != null && !hasMask ) {
             HitByEnemy();
         }
     }
-
+    private void KeysMovePlayer() {
+        verAxis = Input.GetAxis( "Vertical" );
+        horAxis = Input.GetAxis( "Horizontal" );
+        if( verAxis == 0 && horAxis == 0 ) {
+            return;
+        }
+        agent.ResetPath();
+        agent.velocity = new Vector3( -verAxis, 0, horAxis ) * walkSpeed;
+    }
+    
     private void MouseMovePlayer() {
-        if (Input.GetMouseButtonDown(0) && gameManager != null && gameManager.IsGameActive) {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawLine( transform.position, transform.position + agent.velocity, Color.red );
+        if( isSlipping ) {
+            destinationIndicator.SetActive( false );
+            agent.ResetPath();
+            agent.velocity *= slipSpeed;
+        }
+        
+        if( Input.GetMouseButtonDown( 0 ) && gameManager && gameManager.IsGameActive ) {
+            agent.ResetPath();
+            Ray ray = cam.ScreenPointToRay( Input.mousePosition );
             RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit)) {
-                destinationIndicator.SetActive(true);
-                agent.SetDestination(hit.point);
-                destinationIndicator.transform.position = new Vector3(hit.point.x, 0.2f, hit.point.z);
+            
+            if( Physics.Raycast( ray, out hit ) ) {
+                destinationIndicator.SetActive( true );
+                agent.SetDestination( hit.point );
+                destinationIndicator.transform.position = new Vector3( hit.point.x, 0.2f, hit.point.z );
             }
         }
-        if (!agent.hasPath && !agent.pathPending) {
-            destinationIndicator.SetActive(false);
+        if( !agent.hasPath && !agent.pathPending ) {
+            destinationIndicator.SetActive( false );
         }
     }
-
+    
     IEnumerator MaskPowerupCountdownCoroutine() {
-        powerupIndicator.SetActive(true);
+        powerupIndicator.SetActive( true );
         hasMask = true;
-        yield return new WaitForSeconds(7);
-        powerupIndicator.SetActive(false);
+        yield return new WaitForSeconds( 7 );
+        powerupIndicator.SetActive( false );
         hasMask = false;
     }
-
-    private void OnCollisionEnter(Collision collision) {
+    
+    private void OnCollisionEnter( Collision collision ) {
         //Grab the Item
-        if (collision.gameObject.CompareTag("Item")) {
+        if( collision.gameObject.CompareTag( "Item" ) ) {
             Transform parent = collision.gameObject.transform.parent;
-            if (parent == null) {
+            if( parent == null ) {
                 parent = collision.transform;
             }
-            if (PickedUpItem != null) {
-                PickedUpItem(parent.gameObject);
+            if( PickedUpItem != null ) {
+                PickedUpItem( parent.gameObject );
             }
-            audioSource.PlayOneShot(itemPickupSound);
-            Destroy(parent.gameObject);
+            audioSource.PlayOneShot( itemPickupSound );
+            Destroy( parent.gameObject );
         }
-
-        if (collision.gameObject.CompareTag("Mask")) {
-            audioSource.PlayOneShot(powerupPickupSound);
-            Destroy(collision.gameObject);
-            StartCoroutine(MaskPowerupCountdownCoroutine());
+        
+        if( collision.gameObject.CompareTag( "Mask" ) ) {
+            audioSource.PlayOneShot( powerupPickupSound );
+            Destroy( collision.gameObject );
+            StartCoroutine( MaskPowerupCountdownCoroutine() );
         }
-
-        if (collision.gameObject.CompareTag("Trolley")) {
-            audioSource.PlayOneShot(powerupPickupSound);
+        
+        if( collision.gameObject.CompareTag( "Trolley" ) ) {
+            audioSource.PlayOneShot( powerupPickupSound );
             var trans = transform;
             collision.transform.parent = trans;
             collision.transform.rotation = trans.rotation;
             collision.transform.position = trans.position;
         }
-
+        
         //Exit Level
-        if (collision.gameObject.CompareTag("Exit")) {
-            if (TriedToExit != null) {
+        if( collision.gameObject.CompareTag( "Exit" ) ) {
+            if( TriedToExit != null ) {
                 TriedToExit();
             }
+        }
+    }
+    
+    private void OnTriggerEnter( Collider other ) {
+        if( other.gameObject.CompareTag( "Shelf" ) ) {
+            Debug.Log( "Bumped Shelf!!!" );
+            particleSystem.Play();
+        }
+        
+        if( other.gameObject.CompareTag( "Water" ) ) {
+            isSlipping = true;
+            playerAnimator.SetBool( "Slipping_b", true );
+        }
+    }
+    
+    private void OnTriggerExit( Collider other ) {
+        if( other.gameObject.CompareTag( "Water" ) ) {
+            isSlipping = false;
+            playerAnimator.SetBool( "Slipping_b", false );
         }
     }
 }
